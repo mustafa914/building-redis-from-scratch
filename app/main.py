8251a6e2141f90347d4522ae76b4b84ae72f36e9
mustafa914 <mustafa.mohamed1618@gmail.com>
2025-11-27 18:48:00 +0000

    continuously read and respond to commands

--- app/main.py
+++ app/main.py
@@ -9,8 +9,10 @@ def main():
     #
     server_socket = socket.create_server(("localhost", 6379), reuse_port=True)
     
+
+    connection, _ = server_socket.accept() 
     while True:
-        connection, _ = server_socket.accept() 
+        data = connection.recv(1024)
         connection.sendall(b"+PONG\r\n")
 
 
