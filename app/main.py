8d8783e81c41361db900df179eaaec277506846e
mustafa914 <mustafa.mohamed1618@gmail.com>
2025-11-28 23:22:48 +0000

    CLI tests (cli-test-1764372168238)

--- app/main.py
+++ app/main.py
@@ -1,25 +1,31 @@
-import socket  # noqa: F401
-import threading
+import asyncio
 
 
-def main():
+async def main():
     # You can use print statements as follows for debugging, they'll be visible when running tests.
     print("Logs from your program will appear here!")
 
     # Uncomment the code below to pass the first stage
     #
-    server_socket = socket.create_server(("localhost", 6379), reuse_port=True)
-    
-    while True:
-        connection, _ = server_socket.accept()         
-        x = threading.Thread(target=handle_connection, args=(connection,))
-        x.start()
 
-def handle_connection(connection):
+    server_socket = await asyncio.start_server(handle_connection,"localhost", 6379)
+
+    try:
+        await server_socket.serve_forever()
+    finally:
+        server_socket.close()
+        await server_socket.await_closed()
+
+async def handle_connection(reader, writer):
     while True:
-        data = connection.recv(1024)
-        connection.sendall(b"+PONG\r\n")
+        data = await reader.read(1024)
+        if not data:
+            break
+        writer.write(b"+PONG\r\n")
+        await writer.drain()
+    writer.close()
+    await writer.await_closed()
 
 
 if __name__ == "__main__":
-    main()
+    asyncio.run(main())
